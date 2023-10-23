b0dbc3ddab8e4dccef0199a24c26eabb0ecc8963
Raghav <harshit.raghav@capgemini.com>
2023-10-23 11:05:31 +0530

    added headers to dealer

--- Case_Study_Frontend/src/app/services/dealer.service.ts
+++ Case_Study_Frontend/src/app/services/dealer.service.ts
@@ -2,6 +2,11 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Crop } from '../models/crop';
 
+const headers = new HttpHeaders({
+  "Content-Type": "application/json",
+  'Authorization': `Basic ${localStorage.getItem('token')}` 
+});
+
 @Injectable({
   providedIn: 'root'
 })
@@ -11,16 +16,17 @@ export class DealerService {
   constructor(private http:HttpClient) {
   
   }
+  
   url="http://localhost:8090/dealer-service";
   getCrops(){
     console.log(this.http.get<Crop[]>(`${this.url}/fetchCrops`));
     
-    return this.http.get<Crop[]>(`${this.url}/fetchCrops`);
+    return this.http.get<Crop[]>(`${this.url}/fetchCrops`,{headers});
   }
   addToCart(crop:Crop,cropId:any,quantity:number,dealerEmail:string){
-    const headers=new HttpHeaders({
-      "Content-Type": "application/json"
-    });
+    // const headers=new HttpHeaders({
+    //   "Content-Type": "application/json"
+    // });
     return this.http.post(`${this.url}/addToCart/${dealerEmail}/${quantity}`,crop,{headers})
   }
   getToken(){
